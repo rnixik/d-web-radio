1a72537e16ad4b3226a31aa5aed0aa209fccdba2
Roman Nix <rnix@yandex.ru>
2020-04-12 19:42:16 +0700

    Add validation of users for empty logins

--- src/transactions/User/UserValidator.ts
+++ src/transactions/User/UserValidator.ts
@@ -5,12 +5,16 @@ import { User } from '@/models/User'
 
 export class UserValidator implements SpecificValidator {
   public validate (storedTransactions: Transaction[], tx: Transaction): void {
+    const user = tx.model as User
+    if (user.login.length < 1) {
+      throw new Error('Empty login')
+    }
+
     for (const storedTx of storedTransactions) {
       if (storedTx.type !== UserTransactionType.t) {
         continue
       }
       const storedUser = storedTx.model as User
-      const user = tx.model as User
       if (storedUser.publicKey === user.publicKey) {
         throw new Error('User already registered')
       }
