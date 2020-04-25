a9134ff7babb8c284f223fe7203a42b528809866
Roman Nix <rnix@yandex.ru>
2020-04-25 22:49:52 +0700

    Improve user's login validation (length)

--- src/transactions/User/UserValidator.ts
+++ src/transactions/User/UserValidator.ts
@@ -4,10 +4,21 @@ import { UserTransactionType } from '@/transactions/User/UserTransactionType'
 import { User } from '@/models/User'
 
 export class UserValidator implements SpecificValidator {
+  public loginMinLength = 1
+  public loginMaxLength = 12
+
   public validate (storedTransactions: Transaction[], tx: Transaction): void {
     const user = tx.model as User
     if (user.login.length < 1) {
       throw new Error('Empty login')
+    }
+
+    if (user.login.length < this.loginMinLength) {
+      throw new Error('Login is too short')
+    }
+
+    if (user.login.length > this.loginMaxLength) {
+      throw new Error('Login is too long')
     }
 
     for (const storedTx of storedTransactions) {
