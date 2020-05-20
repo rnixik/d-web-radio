8d0ca31776c3e531543e63ed256b2c52476eb71f
Roman Nix <rnix@yandex.ru>
2020-05-20 22:36:22 +0700

    Improve layout of connections page

--- src/components/ManualSignaling.vue
+++ src/components/ManualSignaling.vue
@@ -1,21 +1,42 @@
 <template>
   <div>
-    Send it to other party:<br>
-    <br>
-    <textarea
-      v-if="showOutgoingMessage"
-      v-model="textToSend"
-      class="text-to-copy form-control"
-    ></textarea>
-    <button @click="copy" class="btn btn-secondary">Copy</button><br>
+    <div v-if="!initiated && !showIncomingMessage">
+      <button @click="initiate" class="btn btn-secondary mr-2">I want to initiate</button>
+      <button @click="showIncomingMessage = true" class="btn btn-secondary">Enter message from other party</button>
+    </div>
 
-    <div><button @click="initiate" class="btn btn-secondary">Initiate</button></div>
-    <div>
-      <div>Paste it here:<br>
-        <textarea v-model="remote" class="form-control"></textarea><br/>
-        <button @click="applyRemote" class="btn btn-secondary">Apply remote</button>
+    <div v-if="showOutgoingMessage" class="form-group">
+      <label for="textToSend">Copy and send the message to the other party:</label>
+      <textarea
+        id="textToSend"
+        v-model="textToSend"
+        class="text-to-copy form-control"
+      ></textarea>
+      <button @click="copy" class="btn btn-secondary mt-2">Copy</button>
+    </div>
+
+    <div v-if="showIncomingMessage" class="form-group">
+      <label v-if="initiated" for="incomingMessageInput">
+        Wait for the message from the other party, paste it here and click the button below:
+      </label>
+      <label v-if="!initiated" for="incomingMessageInput">
+        Paste the message from the other party and click the button below:
+      </label>
+      <textarea
+        id="incomingMessageInput"
+        v-model="remote"
+        class="form-control"
+      ></textarea>
+      <button @click="applyRemote" class="btn btn-secondary mt-2">Apply remote</button>
+    </div>
+
+    <div v-if="waitingForConnection" >
+      Waiting for connection...
+      <div class="spinner-grow" role="status">
+        <span class="sr-only">Waiting for connection...</span>
       </div>
     </div>
+
     <div v-if="connectionError" class="alert alert-danger">Connection error</div>
     <div v-if="isConnected" class="alert alert-success">CONNECTED</div>
   </div>
@@ -32,8 +53,11 @@ export default class ManualSignaling extends Vue {
   remote: string = ''
   textToSend: string = ''
   showOutgoingMessage: boolean = false
+  showIncomingMessage: boolean = false
+  initiated: boolean = false
   isConnected: boolean = false
   connectionError: boolean = false
+  waitingForConnection: boolean = false
 
   created (): void {
     this.signaling = new ManualSignalingImplementation()
@@ -42,17 +66,21 @@ export default class ManualSignaling extends Vue {
     })
     this.signaling.bindOnWebRtcConnectionErrorCallback((id: string) => {
       this.connectionError = true
+      this.waitingForConnection = false
     })
 
     const connection = this.connectionsPool!.connect(this.signaling!)
     connection.addOnOpenCallback(() => {
       this.isConnected = true
+      this.waitingForConnection = false
       this.$root.$emit('manualConnected')
     })
   }
 
   initiate (): void {
+    this.initiated = true
     this.showOutgoingMessage = true
+    this.showIncomingMessage = true
     this.signaling!.initiate()
     this.connectionError = false
   }
@@ -70,6 +98,8 @@ export default class ManualSignaling extends Vue {
     this.connectionError = false
     this.showOutgoingMessage = true
     this.signaling!.applyRemoteResponse(JSON.parse(this.remote))
+    this.showIncomingMessage = false
+    this.waitingForConnection = true
   }
 }
 </script>
